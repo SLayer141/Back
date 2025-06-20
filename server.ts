8716d70deca6d28500968293bd0365dbcee75df4
SLayer141 <sonishtthapa1999@gmail.com>
2025-06-21 00:20:33 +0530

    some more changes

--- server.ts
+++ server.ts
@@ -20,7 +20,7 @@ app.post('/', (req: Request, res: Response) => {
 AppDataSource.initialize()
   .then(() => {
     console.log('Database connected');
-    app.listen(port, () => {
+    app.listen(Number(port), '0.0.0.0', () => {
       console.log(`Server is running on port ${port}`);
     });
   })
